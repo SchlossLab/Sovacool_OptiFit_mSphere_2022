54f606c079da9d1a00344cd035ed5a6cb898d950
MBMumphrey <mumphrey@umich.edu>
2018-10-02 15:27:43 -0400

    Added suffix argument for optifit_multi.sh

--- code/analysis/marine_pipeline.sh
+++ code/analysis/marine_pipeline.sh
@@ -9,14 +9,17 @@ TRIMSIZE=$2
 
 #Subset data (optional)
 #Arg to marine_trim.sh is the number of sequences to include in subet
-if [ ! -z "$2" ]
+if [ ! -z "$2" ] #if second command line argument is not an empty string
 then
 	./code/data/marine_trim.sh $TRIMSIZE
+	SUFFIX=$(echo $TRIMSIZE.)
+else
+	SUFFIX=""
 fi
 
 
 #Run optifit iteratively
-./code/analysis/optifit_multi.sh $NUMSEQS
+./code/analysis/optifit_multi.sh $NUMSEQS $SUFFIX
 
 #Plot resulting data
 R -e "source('code/analysis/plot_marine_sensspec.R'); make_plot()"
