b3452af4665c7d08292023039eeccf04328199f8
MBMumphrey <mumphrey@umich.edu>
2018-10-04 15:13:48 -0400

    Made variable names more explicit

--- code/analysis/optifit_marine.sh
+++ code/analysis/optifit_marine.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 #Usage: optifit_marine.sh size suffix
+
 #Required: Size is an int and tells the script how many sequences to cut out to fit against the rest of the sample
+SIZE=$1
 #Optional: Suffix allows you to add an optional suffix after marine. in case there are alternative files to use
+SUFFIX=$2
 
 MARINE=data/marine
 
@@ -28,12 +31,12 @@ MARINE=data/marine
 #cluster.fit(reflist=reference.opti_mcc.list, refcolumn=reference.dist, refcount=reference.count_table, reffasta=reference.fasta, fasta=sample.fasta, count=sample.count_table, column=sample.dist, inputdir=$MARINE, printref=t)"
 
 mothur "#set.dir(output=$MARINE);
-	sub.sample(inputdir=$MARINE, fasta=marine.$2fasta, size=$1);
+	sub.sample(inputdir=$MARINE, fasta=marine.${SUFFIX}fasta, size=$SIZE);
 	list.seqs(fasta=current);
-	get.seqs(accnos=current, count=marine.$2count_table);
+	get.seqs(accnos=current, count=marine.${SUFFIX}count_table);
 	rename.file(fasta=current, count=current, accnos = current, prefix=sample);
 	dist.seqs(fasta=current, cutoff=0.03);
-	remove.seqs(fasta=marine.$2fasta, count=marine.$2count_table, accnos=current);
+	remove.seqs(fasta=marine.${SUFFIX}fasta, count=marine.${SUFFIX}count_table, accnos=current);
 	rename.file(fasta=current, count=current, prefix=reference);
 	dist.seqs(fasta=current, cutoff=0.03);
 	cluster(column=current, count=current);
