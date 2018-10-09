430103a8b923fd05b7e0bd2e8f0e12b4b2062935
MBMumphrey <mumphrey@umich.edu>
2018-10-09 11:00:47 -0400

    Combined 3 graphs into a single one so data can be compared more easily. Also added a filename argument for customization without changing source code.

--- code/analysis/plot_marine_sensspec.R
+++ code/analysis/plot_marine_sensspec.R
@@ -1,47 +1,13 @@
+#Makes MCC plots from the provided sensspec file
+args <- commandArgs(trailingOnly = TRUE)
+
+filename <- args[1]
+
 #Make the plots
-make_plot <- function() {
+make_plot <- function(filename) {
   #Read in accumulated sensspec data created by optifit_multi.sh
-  sensspec <- read.table(file = "data/marine/marine.sensspec.final", header = TRUE)
+  sensspec <- read.table(file = filename, header = TRUE)
   sensspec <- sensspec[, 4:ncol(sensspec)]
-  
-  sensspec_long <- tidyr::gather(sensspec, key = "Score", value = "Value", c(1:11, 13, 14))
-  
-  #MCC from OptiFit
-  fit_mcc <- ggplot2::ggplot(data = sensspec, ggplot2::aes(x = refp, y = mcc)) +
-    ggplot2::geom_jitter(width = 0.1, height = 0) +
-    ggplot2::labs(title = "OptiFit clustering using partial marine dataset") +
-    ggplot2::ylab("MCC") +
-    ggplot2::xlab("Amount of data used as reference") +
-    #ggplot2::scale_y_continuous(breaks = seq(0, 1, by = .1))+
-    #ggplot2::coord_cartesian(ylim = c(0, 1)) +
-    ggplot2::scale_x_continuous(breaks = seq(0, 100, by = 10))
-  
-  ggplot2::ggsave("results/figures/marine/marine_fit_mcc.png", plot = fit_mcc, width = 6.5, height = 4.5, unit="in")
-  
-  #MCC of reference clustered on its own with opticlust
-  ref_mcc <- ggplot2::ggplot(data = sensspec, ggplot2::aes(x = refp, y = refmcc)) +
-    ggplot2::geom_jitter(width = 0.1, height = 0) +
-    ggplot2::labs(title = "OptiFit clustering using partial marine dataset") +
-    ggplot2::ylab("MCC") +
-    ggplot2::xlab("Amount of data used as reference") +
-    #ggplot2::scale_y_continuous(breaks = seq(0, 1, by = .1))+
-    #ggplot2::coord_cartesian(ylim = c(0, 1)) +
-    ggplot2::scale_x_continuous(breaks = seq(0, 100, by = 10))
-  
-  ggplot2::ggsave("results/figures/marine/marine_ref_mcc.png", plot = ref_mcc, width = 6.5, height = 4.5, unit="in")
-  
-  #MCC of sample clustered on its own with opticlust
-  samp_mcc <- ggplot2::ggplot(data = sensspec, ggplot2::aes(x = refp, y = sampmcc)) +
-    ggplot2::geom_jitter(width = 0.1, height = 0) +
-    ggplot2::labs(title = "OptiFit clustering using partial marine dataset") +
-    ggplot2::ylab("MCC") +
-    ggplot2::xlab("Amount of data used as reference") +
-    #ggplot2::scale_y_continuous(breaks = seq(0, 1, by = .1))+
-    #ggplot2::coord_cartesian(ylim = c(0, 1)) +
-    ggplot2::scale_x_continuous(breaks = seq(0, 100, by = 10))
-  
-  ggplot2::ggsave("results/figures/marine/marine_samp_mcc.png", plot = samp_mcc, width = 6.5, height = 4.5, unit="in")
-  
   sensspec <- tidyr::gather(sensspec, key = "mcc_type", value = "mcc", c(12, 15, 16))
   
   combo_mcc <- ggplot2::ggplot(data=sensspec, ggplot2::aes(x = refp, y = mcc, color = mcc_type)) +
@@ -53,7 +19,10 @@ make_plot <- function() {
     ggplot2::coord_cartesian(ylim = c(.75, 1)) +
     ggplot2::scale_x_continuous(breaks = seq(0, 100, by = 10))
   
-  ggplot2::ggsave("results/figures/marine/marine_all_mcc.png", plot = combo_mcc, width = 6.5, height = 4.5, unit = "in")
+  ggplot2::ggsave(paste("results/figures/marine/", basename(filename), ".mcc.png", sep = ""),
+                  plot = combo_mcc, width = 6.5, height = 4.5, unit = "in")
+  
+  sensspec_long <- tidyr::gather(sensspec, key = "Score", value = "Value", c(1:11, 13, 14))
   
   #Plot mcc values against various other scores from the confusion matrix
   mcc_v_cm <- ggplot2::ggplot(data=sensspec_long, ggplot2::aes(x = Value, y = mcc)) +
@@ -62,7 +31,8 @@ make_plot <- function() {
     ggplot2::xlab("Amount of data used as reference") +
     ggplot2::facet_wrap(~ Score, scales="free")
   
-  ggplot2::ggsave("results/figures/marine/marine_mcc_v_cm.png", plot=mcc_v_cm, width = 10, height = 7, unit="in")
+  ggplot2::ggsave(paste("results/figures/marine/", basename(filename), ".mcc_v_cm.png", sep = ""),
+                  plot=mcc_v_cm, width = 10, height = 7, unit="in")
 }
 
-make_plot()
+make_plot(filename)
