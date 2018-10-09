60e6ccd9138600caf26489bc237708fa6cc63a07
MBMumphrey <mumphrey@umich.edu>
2018-10-09 11:21:57 -0400

    Made one graph flexible scaling, and one that shows entire scale from 0 to 1.

--- code/analysis/plot_marine_sensspec.R
+++ code/analysis/plot_marine_sensspec.R
@@ -16,11 +16,23 @@ make_plot <- function(filename) {
     ggplot2::ylab("MCC") +
     ggplot2::xlab("Amount of data used as reference") +
     #ggplot2::scale_y_continuous(breaks = seq(0, 1, by = .1))+
-    ggplot2::coord_cartesian(ylim = c(.75, 1)) +
+    #ggplot2::coord_cartesian(ylim = c(.75, 1)) +
     ggplot2::scale_x_continuous(breaks = seq(0, 100, by = 10))
   
   ggplot2::ggsave(paste("results/figures/marine/", basename(filename), ".mcc.png", sep = ""),
                   plot = combo_mcc, width = 6.5, height = 4.5, unit = "in")
+  
+  combo_mcc_full <- ggplot2::ggplot(data=sensspec, ggplot2::aes(x = refp, y = mcc, color = mcc_type)) +
+    ggplot2::geom_jitter(width = 1, height = 0) +
+    ggplot2::labs(title = "OptiFit clustering using partial marine dataset") +
+    ggplot2::ylab("MCC") +
+    ggplot2::xlab("Amount of data used as reference") +
+    #ggplot2::scale_y_continuous(breaks = seq(0, 1, by = .1))+
+    ggplot2::coord_cartesian(ylim = c(0, 1)) +
+    ggplot2::scale_x_continuous(breaks = seq(0, 100, by = 10))
+  
+  ggplot2::ggsave(paste("results/figures/marine/", basename(filename), ".mcc.full.png", sep = ""),
+                  plot = combo_mcc_full, width = 6.5, height = 4.5, unit = "in")
   
   sensspec_long <- tidyr::gather(sensspec, key = "Score", value = "Value", c(1:11, 13, 14))
   
