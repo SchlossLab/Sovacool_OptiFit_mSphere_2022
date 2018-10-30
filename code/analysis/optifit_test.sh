3085cd7a9ad97f2a85973a92a17f321a523880f3
MBMumphrey <mumphrey@umich.edu>
2018-10-30 13:27:47 -0400

    Updated to use weighted_subsample to choose subsamples instead of mothur, to allow for weighted subsampling

--- code/analysis/optifit_test.sh
+++ code/analysis/optifit_test.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-#Usage: optifit_test.sh INPUTDIR OUTPUTDIR DATASET SIZE SEED PREFIX
+#Usage: optifit_test.sh INPUTDIR OUTPUTDIR DATASET SIZE WEIGHT SEED PREFIX
 
 INPUTDIR=$1
-OUTPUTDIR=$2 #Directory to put output in
-DATASET=$3
+OUTPUTDIR=$2
+DATASET=$3 #Required: Dataset to use (human, mice, marine, soil)
 SIZE=$4 #Required: Size is an int and tells the script how many sequences to cut out to fit against the rest of the sample
-SEED1=$5 #Required: Random seed for mothur to use when cutting the data
-SEED2=$6 #Required: Random seed for mothur to use when clustering
-PREFIX=$7 #Optional: PREFIX allows you to add an optional PREFIX before DATASET. in case there are alternative files to use
+WEIGHT=$5 #Required: Boolean, whether or not to weight the subsampling by abundance
+SEED1=$6 #Required: Random seed for mothur to use when cutting the data
+SEED2=$7 #Required: Random seed for mothur to use when clustering
+PREFIX=$8 #Optional: PREFIX allows you to add an optional PREFIX before DATASET. in case there are alternative files to use
 
 mkdir -p ${OUTPUTDIR}
 
+Rscript code/analysis/weighted_subsample.R ${INPUTDIR}${PREFIX}${DATASET}.count_table $SIZE $WEIGHT
+
 mothur "#set.seed(seed=${SEED1});
-	set.dir(output=${OUTPUTDIR});
-	sub.sample(inputdir=${INPUTDIR}, fasta=${PREFIX}${DATASET}.fasta, size=$SIZE);
-	list.seqs(fasta=current);
-	get.seqs(accnos=current, count=${PREFIX}${DATASET}.count_table);
+	set.dir(output=${OUTPUTDIR}, input=${INPUTDIR});
+	get.seqs(accnos=sample.accnos, fasta=${PREFIX}${DATASET}.fasta);
+	get.seqs(accnos=sample.accnos, count=${PREFIX}${DATASET}.count_table);
 	get.dists(column=${PREFIX}${DATASET}.dist, accnos=current);
 	rename.file(fasta=current, count=current, accnos = current, column=current, prefix=${PREFIX}sample);
 	set.seed(seed=${SEED2});
 	cluster(column=current, count=current);
 	set.seed(seed=${SEED1});
-	remove.seqs(fasta=${PREFIX}${DATASET}.fasta, count=${PREFIX}${DATASET}.count_table, accnos=current);
+	remove.seqs(fasta=${PREFIX}${DATASET}.fasta, count=${PREFIX}${DATASET}.count_table, accnos=${PREFIX}sample.accnos);
 	list.seqs(fasta=current);
 	get.dists(column=${PREFIX}${DATASET}.dist, accnos=current);
 	rename.file(fasta=current, count=current, column=current, accnos=current, prefix=${PREFIX}reference);
