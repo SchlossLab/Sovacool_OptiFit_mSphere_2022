5e819743f733334346e83a4883bfacc65b4f39ad
MBMumphrey <mumphrey@umich.edu>
2018-10-30 13:27:10 -0400

    Corrected variable names and fixed command line args to be proper data types

--- code/analysis/weighted_subsample.R
+++ code/analysis/weighted_subsample.R
@@ -2,7 +2,6 @@
 #
 #Takes a count_table file and takes a random subsample based on abundance
 #Used to separate a dataset into a reference and a sample for optifit
-#We consider the defined subsample to be the reference, and the leftovers to be the sample
 #
 #Args:
 # filename: path to a count_table file
@@ -14,8 +13,8 @@ require(readr)
 args <- commandArgs(trailingOnly = TRUE)
 
 filename <- args[1]
-size <- args[2]
-weight <- args[3]
+size <- as.numeric(args[2])
+weight <- as.logical(args[3])
 
 #filename <- "data\\marine\\marine.count_table"
 #size <- 1000
@@ -25,18 +24,18 @@ count_table <- readr::read_tsv(file = filename)
 
 if (weight) {
   #Take a random subsampling weighted by total abundance
-  reference <- dplyr::sample_n(count_table, size, weight = count_table$total)
+  sample <- dplyr::sample_n(count_table, size, weight = count_table$total)
   
 } else {
   #Take an unweighted random sample
-  reference <- dplyr::sample_n(count_table, size)
+  sample <- dplyr::sample_n(count_table, size)
 }
 
-sample <- dplyr::filter(count_table, !(Representative_Sequence %in% reference$Representative_Sequence))
+reference <- dplyr::filter(count_table, !(Representative_Sequence %in% sample$Representative_Sequence))
 
 #Create .accnos files for both the sample, and the sample_complement for later use in mothur
 #.accnos files contain a column of sequence names and nothing else
-readr::write_tsv(dplyr::select(reference, "Representative_Sequence"), col_names = FALSE,
-                 path = paste(dirname(filename), "reference.accnos", sep = "/"))
 readr::write_tsv(dplyr::select(sample, "Representative_Sequence"), col_names = FALSE,
                  path = paste(dirname(filename), "sample.accnos", sep = "/"))
+#readr::write_tsv(dplyr::select(reference, "Representative_Sequence"), col_names = FALSE,
+#                 path = paste(dirname(filename), "reference.accnos", sep = "/"))
