886025eb5888582a83d47ae530573eab84a70b0c
MBMumphrey <mumphrey@umich.edu>
2018-10-30 11:28:31 -0400

    Added R script to take a subsample of a count_table file weighted by the abundance of each sample

---
+++ code/analysis/weighted_subsample.R
@@ -0,0 +1,34 @@
+#Usage: Rscript weighted_subsample.R filename size weight
+#
+#Takes a count_table file and takes a random subsample based on abundance
+#Used to separate a dataset into a reference and a sample for optifit
+#We consider the defined subsample to be the reference, and the leftovers to be the sample
+#
+#Args:
+# filename: path to a count_table file
+# size: size of subsample to take
+# weight: weight of abundance when taking random sample
+require(dplyr)
+require(readr)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+filename <- args[1]
+size <- args[2]
+weight <- args[3]
+
+filename <- "data\\marine\\marine.count_table"
+size <- 1000
+
+count_table <- readr::read_tsv(file = filename)
+
+#Take a random subsampling weighted by total abundance
+reference <- dplyr::sample_n(count_table, size, weight = count_table$total)
+sample <- dplyr::filter(count_table, !(Representative_Sequence %in% reference$Representative_Sequence))
+
+#Create .accnos files for both the sample, and the sample_complement for later use in mothur
+#.accnos files contain a column of sequence names and nothing else
+readr::write_tsv(dplyr::select(reference, "Representative_Sequence"), col_names = FALSE,
+                 path = paste(dirname(filename), "reference.accnos", sep = "/"))
+readr::write_tsv(dplyr::select(sample, "Representative_Sequence"), col_names = FALSE,
+                 path = paste(dirname(filename), "sample.accnos", sep = "/"))
