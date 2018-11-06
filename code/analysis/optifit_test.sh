d50ddbbf07aef7adec6813e3ced7fef0f3f60112
Kelly Sovacool <kellysovacool@gmail.com>
2018-11-06 12:17:15 -0500

    add comments

--- code/analysis/optifit_test.sh
+++ code/analysis/optifit_test.sh
@@ -7,14 +7,15 @@ OUTPUTDIR=$2
 DATASET=$3 #Required: Dataset to use (human, mice, marine, soil)
 SIZE=$4 #Required: Size is an int and tells the script how many sequences to cut out to fit against the rest of the sample
 WEIGHT=$5 #Required: How to weight the subsample (none, sample_abundance, sample_dists, ref_abundance, ref_dists)
-SEED1=$6 #Required: Random seed for mothur to use when cutting the data
-SEED2=$7 #Required: Random seed for mothur to use when clustering
+SEED1=$6 #Required: Random seed for mothur to use when cutting the data - SAME CUT (replicate)
+SEED2=$7 #Required: Random seed for mothur to use when clustering - DIFFERENT SEED SO OPTICLUST RUNS ARE DIFFERENT (iteration)
 PREFIX=$8 #Optional: PREFIX allows you to add an optional PREFIX before DATASET. in case there are alternative files to use
 
 mkdir -p ${OUTPUTDIR}
 
 Rscript code/analysis/weighted_subsample.R ${INPUTDIR}${PREFIX}${DATASET}.count_table $OUTPUTDIR $SIZE $WEIGHT ${INPUTDIR}${PREFIX}${DATASET}.dist
-
+# all seed1 stuff: do once for each iter
+# all seed2 stuff: do <replicates>
 mothur "#set.seed(seed=${SEED1});
 	set.dir(output=${OUTPUTDIR}, input=${INPUTDIR});
 	get.seqs(accnos=${OUTPUTDIR}sample.accnos, fasta=${PREFIX}${DATASET}.fasta);
