3c0557d6e04193ccd1747a7d7b0414a47f6ac80c
Kelly Sovacool <kellysovacool@gmail.com>
2018-11-13 19:29:09 -0500

    fix filter: 50% -> 0.5

--- code/analysis/plot_sensspec.R
+++ code/analysis/plot_sensspec.R
@@ -35,7 +35,7 @@ make_plot <- function(dataset, input_filename, output_filenames) {
   ggplot2::ggsave(output_filenames[["mcc_full"]],
                   plot = combo_mcc_full, width = 6.5, height = 4.5, unit = "in")
   
-  iters <- ggplot2::ggplot(data=dplyr::filter(sensspec, reference_fraction == 50 & (type == "SAMP" | type == "SAMP_O_NOREF")),
+  iters <- ggplot2::ggplot(data=dplyr::filter(sensspec, reference_fraction == .5 & (type == "SAMP" | type == "SAMP_O_NOREF")),
                            ggplot2::aes(x = refpi, y = mcc, color = type)) +
     ggplot2::geom_jitter(width = .1, height = 0, size = 3) +
     ggplot2::labs(title = title) +
