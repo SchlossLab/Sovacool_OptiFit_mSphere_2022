0c4138b93728bd45fdd9340751a6ec0ba5e17732
Kelly Sovacool <kellysovacool@gmail.com>
2019-11-26 14:58:13 -0500

    Setup prep_samples subworkflow on greatLakes

---
+++ subworkflows/1_prep_samples/code/slurm/debug.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##################
+####  Slurm preamble
+
+#### #### ####  These are the most frequently changing options
+
+####  Job name
+#SBATCH --job-name=ofa-debug
+
+####  Request resources here
+####    These are typically, number of processors, amount of memory,
+####    an the amount of time a job requires.  May include processor
+####    type, too.
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1GB
+#SBATCH --time=24:00:00
+
+#SBATCH --output=log/hpc/slurm-%j.out
+
+####  Slurm account and partition specification here
+####    These will change if you work on multiple projects, or need
+####    special hardware, like large memory nodes or GPUs.
+
+#SBATCH --account=pschloss
+#SBATCH --partition=standard
+
+#### #### ####  These are the least frequently changing options
+
+####  Your e-mail address and when you want e-mail
+
+#SBATCH --mail-user=sovacool@umich.edu
+#SBATCH --mail-type=BEGIN,END
+
+snakemake --profile config/slurm --latency-wait 30 data/human/human.files data/marine/marine.files
