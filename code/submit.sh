fdd0c0fd15da6493016ec184f81644b6f01a6605
Kelly Sovacool <kellysovacool@gmail.com>
2019-12-12 21:24:00 -0500

    Run prep_samples subworkflow

--- code/submit.sh
+++ code/submit.sh
@@ -16,7 +16,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1GB
+#SBATCH --mem-per-cpu=2GB
 #SBATCH --time=24:00:00
 
 #SBATCH --output=log/hpc/slurm-%j.out
