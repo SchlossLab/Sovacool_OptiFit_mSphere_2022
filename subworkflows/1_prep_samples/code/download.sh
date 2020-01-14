c59bb85bd09700e216fefed4cf995cd452a87cf0
Kelly Sovacool <kellysovacool@gmail.com>
2020-01-14 12:10:30 -0500

    Don't use download.sh for mouse dataset

--- subworkflows/1_prep_samples/code/download.sh
+++ subworkflows/1_prep_samples/code/download.sh
@@ -1,14 +1,8 @@
 source /etc/profile.d/http_proxy.sh  # required for internet on the Great Lakes cluster
 list_file=$1
 outdir=$2
-if [[ $outdir == *"mouse"* ]]; then
-    wget -N -P $outdir http://www.mothur.org/MiSeqDevelopmentData/StabilityNoMetaG.tar
-    tar -xvf $outdir/StabilityNoMetaG.tar -C $outdir/
-    rm $outdir/StabilityNoMetaG.tar
-else
-    for SRA in $(cat $list_file)
-    do
-        fasterq-dump --split-files $SRA -O $outdir
-    done
-    gzip ${outdir}/*
-fi
+for SRA in $(cat $list_file)
+do
+    fasterq-dump --split-files $SRA -O $outdir
+done
+gzip ${outdir}/*
