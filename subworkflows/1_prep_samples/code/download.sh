f2f709289533284c44970011856aebcd37015384
Kelly Sovacool <kellysovacool@gmail.com>
2020-02-03 12:35:20 -0500

    Download SRAs as separate jobs

--- subworkflows/1_prep_samples/code/download.sh
+++ subworkflows/1_prep_samples/code/download.sh
@@ -1,9 +1,7 @@
 source /etc/profile.d/http_proxy.sh  # required for internet on the Great Lakes cluster
-list_file=$1
+sra=$1
 outdir=$2
-prefetch --option-file $list_file
-for SRA in $(cat $list_file)
-do
-    fasterq-dump --split-files $SRA -O $outdir
-done
-gzip ${outdir}/*
+
+prefetch $sra
+fasterq-dump --split-files $sra -O $outdir
+gzip ${outdir}/*.fastq
