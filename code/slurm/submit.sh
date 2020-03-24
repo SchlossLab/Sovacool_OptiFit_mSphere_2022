d37281f9ad1aea5450d9a7309dac2ba73a91f035
Kelly Sovacool <kellysovacool@gmail.com>
2020-03-24 13:29:48 -0400

    Decrease mem for master job

--- code/slurm/submit.sh
+++ code/slurm/submit.sh
@@ -16,7 +16,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2GB
+#SBATCH --mem-per-cpu=1GB
 #SBATCH --time=96:00:00
 
 #SBATCH --output=log/hpc/slurm-%j_%x.out
