bf24c0de1d44911b813a7977d0595f2ffa5dba50
Kelly Sovacool <kellysovacool@gmail.com>
2020-05-28 13:51:01 -0400

    Use pipes

--- subworkflows/2_fit_reference_db/code/merge_results.R
+++ subworkflows/2_fit_reference_db/code/merge_results.R
@@ -3,9 +3,11 @@ library(purrr)
 library(readr)
 
 merge_results <- function(key) {
-  infilename <- snakemake@input[[key]]
-  dfs <- map(infilenames, read_tsv)
-  bind_rows(dfs) %>% write_tsv(snakemake@output[[key]])
+  snakemake@input[[key]] %>% 
+    map(read_tsv) %>% 
+    bind_rows() %>% 
+    write_tsv(snakemake@output[[key]])
 }
+
 merge_results("sensspec")
 merge_results("bench")
