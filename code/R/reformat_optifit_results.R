c68a99fce5434faf423fcad25a5ea74923348641
Kelly Sovacool <kellysovacool@gmail.com>
2020-08-24 09:24:37 -0400

    Save R messages / warnings to logfile

--- code/R/reformat_optifit_results.R
+++ code/R/reformat_optifit_results.R
@@ -25,5 +25,8 @@ reformat <- function(key) {
     write_tsv(snakemake@output[[key]])
 }
 
+log <- file(snakemake@log[[1]])
+sink(log)
+sink(log, append = TRUE, type = "message")
 reformat("sensspec")
 reformat("bench")
