0abdaff9dc7424686ef339825e4063ae2ca77993
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-07 16:42:16 -0400

    Refactor merging

--- code/R/functions.R
+++ code/R/functions.R
@@ -1,17 +1,6 @@
-library(dplyr)
-library(purrr)
-library(readr)
-
 if (!is.null(snakemake@log)) {
   log_filepath <- snakemake@log[1][[1]]
   log <- file(log_filepath, open = "wt")
   sink(log, append = TRUE)
   sink(log, append = TRUE, type = "message")
-}
-
-merge_results <- function(key) {
-  snakemake@input[[key]] %>%
-    map(read_tsv) %>%
-    bind_rows() %>%
-    write_tsv(snakemake@output[[key]])
 }
--- code/R/merge_results.R
+++ code/R/merge_results.R
@@ -1,4 +1,13 @@
+library(dplyr)
+library(purrr)
+library(readr)
+
 source(snakemake@input[["fcns"]])
-merge_results("sensspec")
-merge_results("bench")
-merge_results("mapped")
+
+merge_results <- function(key) {
+  snakemake@input[[key]] %>%
+    map(read_tsv) %>%
+    bind_rows() %>%
+    write_tsv(snakemake@output[[key]])
+}
+merge_results("tsv")
