b1f8939d42b8b736eb94dfcd6246767c992668f6
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-07 14:30:50 -0400

    Merge fraction reads mapped results with R

--- code/R/merge_results.R
+++ code/R/merge_results.R
@@ -1,13 +1,4 @@
-library(dplyr)
-library(purrr)
-library(readr)
-
-merge_results <- function(key) {
-  snakemake@input[[key]] %>%
-    map(read_tsv) %>%
-    bind_rows() %>%
-    write_tsv(snakemake@output[[key]])
-}
-
+source(snakemake@input[['fcns']])
 merge_results("sensspec")
 merge_results("bench")
+merge_results('mapped')
