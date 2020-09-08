c029ac3c0143422b00d46146723367f651c4f980
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-08 11:03:48 -0400

    Consolidate slurm profile

--- subworkflows/2_fit_reference_db/code/submit_sub2.sh
+++ subworkflows/2_fit_reference_db/code/submit_sub2.sh
@@ -35,4 +35,4 @@
 #SBATCH --mail-user=sovacool@umich.edu
 #SBATCH --mail-type=BEGIN,END
 
-time snakemake --profile config/slurm --latency-wait 90
+time snakemake --profile ../../config/slurm --latency-wait 90
