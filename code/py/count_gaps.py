44d5e6d0c10a4fdad993b927dc9e3e422b3ff29e
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-11 14:12:18 -0400

    Count total chars too

--- code/py/count_gaps.py
+++ code/py/count_gaps.py
@@ -3,16 +3,16 @@
 
 def main():
     with open(snakemake.input.fasta, "r") as infile:
-        n_gaps = count_gaps(infile)
+        n_gaps, total_chars = count_gaps(infile)
     with open(snakemake.output.txt, "w") as outfile:
-        outfile.write("dataset\tn_gaps\n")
-        outfile.write(f"{snakemake.wildcards.dataset}\t{n_gaps}\n")
+        outfile.write("dataset\tn_gaps\ttotal_chars\n")
+        outfile.write(f"{snakemake.wildcards.dataset}\t{n_gaps}\t{total_chars}\n")
 
-
-def count_gaps(input_filename, gap_char="-", header_char=">"):
-    return sum(
-        line.count(gap_char) for line in infile if not line.startswith(header_char)
-    )
+def count_gaps(infile, gap_char="-", header_char=">"):
+    seqs_cat = ''.join(line.strip() for line in infile if not line.startswith(header_char))
+    n_gaps = seqs_cat.count(gap_char)
+    total_chars = len(seqs_cat)
+    return n_gaps, seqs_cat
 
 
 main()
