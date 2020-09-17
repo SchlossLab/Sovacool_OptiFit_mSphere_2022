fe03bbeeec9809778db27f84caa227d99f558a7e
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-17 15:49:36 -0400

    Keep total column

--- code/R/mutate_ref_count.R
+++ code/R/mutate_ref_count.R
@@ -1,5 +1,5 @@
 library(dplyr)
 readr::read_tsv(snakemake@input[["count"]]) %>%
   mutate(dummyRefGroup = total) %>%
-  select(Representative_Sequence, dummyRefGroup) %>%
+  select(Representative_Sequence, total, dummyRefGroup) %>%
   readr::write_tsv(snakemake@output[["count"]])
