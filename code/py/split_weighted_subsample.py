231535d0a1ebfc9af9c3aa9ca0609cf35ffabe1f
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-18 13:19:46 -0400

    Try simpler logging method

--- code/py/split_weighted_subsample.py
+++ code/py/split_weighted_subsample.py
@@ -8,21 +8,8 @@ import shutil
 
 
 def main():
-    print(snakemake.log)
-    logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
-    filename=snakemake.log,
-    filemode='a'
-    )
-
-    stdout_logger = logging.getLogger('STDOUT')
-    sl = StreamToLogger(stdout_logger, logging.INFO)
-    sys.stdout = sl
-
-    stderr_logger = logging.getLogger('STDERR')
-    sl = StreamToLogger(stderr_logger, logging.ERROR)
-    sys.stderr = sl
+    fh = logging.FileHandler(snakemake.log)
+    fh.setLevel(logging.DEBUG)
 
     np.random.seed(int(snakemake.wildcards.seed))
 
@@ -140,18 +127,5 @@ class SeqList:
             for seq_id in self.ids:
                 outfile.write(f"{seq_id}\n")
 
-
-class StreamToLogger(object): # from http://techies-world.com/how-to-redirect-stdout-and-stderr-to-a-logger-in-python/
-      """
-      Fake file-like stream object that redirects writes to a logger instance.
-      """
-      def __init__(self, logger, log_level=logging.INFO):
-            self.logger = logger
-            self.log_level = log_level
-            self.linebuf = ''
-
-      def write(self, buf):
-            for line in buf.rstrip().splitlines():
-                  self.logger.log(self.log_level, line.rstrip())
 
 main()
