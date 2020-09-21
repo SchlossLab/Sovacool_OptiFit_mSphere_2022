4f775251d627972d8703a4c9f8d4ed34797689e3
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-20 21:44:25 -0400

    Code to full-join everything to one tsv

---
+++ code/R/full_join_tsvs.R
@@ -0,0 +1,7 @@
+source(snakemake@input[["fcns"]])
+
+log_smk()
+snakemake@input[['tsv']] %>%
+    purrr::map(readr::read_tsv) %>%
+    purrr::reduce(dplyr::full_join) %>%
+    readr::write_tsv(snakemake@output[['tsv']])
