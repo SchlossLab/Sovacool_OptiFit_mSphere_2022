6940ce459e250dfca186c4a8af3f931b9d6e1864
Kelly Sovacool <kellysovacool@gmail.com>
2020-10-01 17:07:04 -0400

    Support fitpercent wildcard

--- code/py/fraction_reads_mapped.py
+++ code/py/fraction_reads_mapped.py
@@ -19,17 +19,29 @@ def main():  # TODO: write to log file
         len(mapped_reads.intersection(all_reads)) / len(all_reads), 3
     )
     wildcards = snakemake.wildcards
-    data_str = (
+    if "sample_frac" in wildcards.__dict__.keys():
+        data_str = (
         f"{wildcards.dataset}\t{wildcards.seed}\t{wildcards.method}\t{wildcards.printref}\t{fraction_mapped}\t{wildcards.sample_frac}\t{wildcards.ref_frac}\t{wildcards.ref_weight}\n"
-        if "sample_frac" in wildcards.__dict__.keys()
-        else f"{wildcards.dataset}\t{wildcards.ref}\t{wildcards.region}\t{wildcards.seed}\t{wildcards.method}\t{wildcards.printref}\t{fraction_mapped}\n"
-    )
-    header_line = (
-        "dataset\tseed\tmethod\tprintref\tfraction_mapped\tsample_frac\tref_frac\tref_weight\n"
-        if "sample_frac" in wildcards.__dict__.keys()
-        else "dataset\tref\tregion\tseed\tmethod\tprintref\tfraction_mapped\n"
-    )
+                )
+        header_line = (
+            "dataset\tseed\tmethod\tprintref\tfraction_mapped\tsample_frac\tref_frac\tref_weight\n"
 
+        )
+    elif 'fitpercent' in wildcards.__dict__.keys():
+        data_str = (
+        f"{wildcards.dataset}\t{wildcards.seed}\t{wildcards.method}\t{wildcards.printref}\t{fraction_mapped}\t{wildcards.fitpercent}\t{wildcards.ref_weight}\n"
+                )
+        header_line = (
+            "dataset\tseed\tmethod\tprintref\tfraction_mapped\tfitpercent\tref_weight\n"
+
+        )
+    else:
+        data_str = (
+        f"{wildcards.dataset}\t{wildcards.ref}\t{wildcards.region}\t{wildcards.seed}\t{wildcards.method}\t{wildcards.printref}\t{fraction_mapped}\n"
+        )
+        header_line = (
+        "dataset\tref\tregion\tseed\tmethod\tprintref\tfraction_mapped\n"
+        )
     with open(snakemake.output.txt, "w") as output_file:
         output_file.write(header_line)
         output_file.write(data_str)
