21382cface81247b084f212779ca4dc54b7fb325
Kelly Sovacool <kellysovacool@gmail.com>
2020-10-04 16:48:20 -0400

    Just pick one if there's a tie

--- code/R/pick_best_list.R
+++ code/R/pick_best_list.R
@@ -4,10 +4,11 @@ source(snakemake@input[["fcns"]])
 log_smk()
 
 sensspec <- snakemake@input[["sensspec"]]
-max_iter <- sensspec %>%
+best_iter <- sensspec %>%
   read_tsv() %>%
   dplyr::top_n(1, mcc) %>%
-  pull(iter)
-best_list_file <- snakemake@input[["lists"]][[max_iter]]
+  pull(iter) %>%
+  .[1]
+best_list_file <- snakemake@input[["lists"]][[best_iter]]
 message(paste("Copying best list file:", best_list_file))
 file.copy(best_list_file, snakemake@output[["list"]])
