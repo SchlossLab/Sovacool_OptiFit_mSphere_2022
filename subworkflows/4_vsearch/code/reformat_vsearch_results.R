c1c2d57af95a309031a5f205ea0b263083f8b9fd
Kelly Sovacool <kellysovacool@gmail.com>
2020-10-27 14:16:59 -0400

    Just use the greengenes db for ref clustering

--- subworkflows/4_vsearch/code/reformat_vsearch_results.R
+++ subworkflows/4_vsearch/code/reformat_vsearch_results.R
@@ -2,8 +2,6 @@ source(snakemake@input[["fcns"]])
 
 log_smk()
 
-dsrr <- strsplit(snakemake@wildcards[["dataset_ref_region"]], "_") %>% unlist()
-
 sapply(c("bench", "sensspec", "div", "map"), function(x) snakemake@input[[x]]) %>%
   map(read_tsv) %>%
   reduce(bind_cols) %>%
@@ -12,12 +10,9 @@ sapply(c("bench", "sensspec", "div", "map"), function(x) snakemake@input[[x]]) %
     method = snakemake@wildcards[["method"]],
     ref = ifelse(method == "de_novo",
       NA,
-      dsrr[[2]]
+      "gg"
     ),
-    region = ifelse(method == "de_novo",
-      NA,
-      paste(dsrr[[3]], dsrr[[4]], sep = "_")
-    ),
+    region = NA,
     tool = "vsearch"
   ) %>%
   write_tsv(snakemake@output[["tsv"]])
