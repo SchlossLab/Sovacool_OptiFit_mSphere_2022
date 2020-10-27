7a7eafa96cbab12826b3c0a214a8172d72ec6d6f
Kelly Sovacool <kellysovacool@gmail.com>
2020-10-27 10:27:32 -0400

    Make seq names in count table match fasta headers (no dots or hyphens)

--- subworkflows/4_vsearch/code/prep_count_table.R
+++ subworkflows/4_vsearch/code/prep_count_table.R
@@ -1,8 +1,8 @@
 library(dplyr)
 readr::read_tsv(snakemake@input[["count"]]) %>%
   mutate(
-    dummyRefGroup = total,
+    dummyRefGroup = total, # vsearch doesn't support dots or hyphens in seq names
     Representative_Sequence = gsub("[\\.-]", "_", Representative_Sequence)
   ) %>%
-  select(Representative_Sequence, dummyRefGroup) %>%
+  select(Representative_Sequence, total, dummyRefGroup) %>%
   readr::write_tsv(snakemake@output[["count"]])
