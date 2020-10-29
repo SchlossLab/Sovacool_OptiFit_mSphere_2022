a715f29949ecaf2c47982bebf86d712ef6c9db6b
Kelly Sovacool <kellysovacool@gmail.com>
2020-10-29 11:03:18 -0400

    fix wildcards

--- code/R/functions.R
+++ code/R/functions.R
@@ -7,7 +7,7 @@ library(readr)
 #' @export
 #'
 log_smk <- function() {
-  if (exists("snakemake") & length(snakemake@log) != 0) {
+  if (exists('snakemake') & length(snakemake@log) != 0) {
     log <- file(snakemake@log[1][[1]], open = "wt")
     sink(log, append = TRUE)
     sink(log, append = TRUE, type = "message")
@@ -25,30 +25,5 @@ rbind_all <- function(key) {
   snakemake@input[[key]] %>%
     map(read_tsv) %>%
     bind_rows() %>%
-    write_tsv(snakemake@output[[key]])
-}
-
-mutate_columns <- function(df_orig) {
-  df_new <- df_orig %>% mutate(
-    dataset = snakemake@params[["dataset"]],
-    ref = snakemake@params[["ref"]],
-    region = snakemake@params[["region"]],
-    seed = snakemake@params[["seed"]],
-    method = snakemake@params[["method"]],
-    printref = snakemake@params[["printref"]]
-  )
-  if (all(c("ref_weight", "ref_frac", "sample_frac") %in% names(snakemake@params))) {
-    df_new <- df_new %>% mutate(
-      ref_weight = snakemake@params[["ref_weight"]],
-      ref_frac = snakemake@params[["ref_frac"]],
-      sample_frac = snakemake@params[["sample_frac"]]
-    )
-  }
-  return(df_new)
-}
-
-reformat <- function(key) {
-  read_tsv(snakemake@input[[key]]) %>%
-    mutate_columns() %>%
     write_tsv(snakemake@output[[key]])
 }
