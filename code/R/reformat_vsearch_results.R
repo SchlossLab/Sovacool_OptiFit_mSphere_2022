1b006740afa2ff2208f84f2ec959497a1ba82abc
Kelly Sovacool <kellysovacool@gmail.com>
2020-11-02 13:58:05 -0500

    purrr::map_dfc()

--- code/R/reformat_vsearch_results.R
+++ code/R/reformat_vsearch_results.R
@@ -3,8 +3,7 @@ source(snakemake@input[["fcns"]])
 log_smk()
 
 sapply(c("bench", "sensspec", "div", "map"), function(x) snakemake@input[[x]]) %>%
-  map(read_tsv) %>%
-  reduce(bind_cols) %>%
+  map_dfc(read_tsv) %>%
   mutate(
     dataset = snakemake@wildcards[["dataset"]],
     method = snakemake@wildcards[["method"]],
