b4f3abb0fa41bb6204a3f08482ed85c113afafa6
Kelly Sovacool <kellysovacool@gmail.com>
2020-11-09 13:51:29 -0500

    Don't forget the header row 🤦🏻‍♀️

--- code/py/rbind_files.py
+++ code/py/rbind_files.py
@@ -1,8 +1,13 @@
 # perform row bind, but don't hold data in memory.
-# assumes all tsv files have the same columns and exactly one row.
+# assumes all tsv files have the same columns.
 
 with open(snakemake.output.tsv, 'w') as outfile:
+    header_row = ''
     for infilename in snakemake.input.tsv:
         with open(infilename, 'r') as infile:
-            next(infile) # throw out header row
-            outfile.write(next(infile))
+            line = next(infile) # throw out header row except first time
+            if not header_row:
+                header_row = line
+                outfile.write(header_row)
+            for line in infile:
+                outfile.write(line)
