5a84b9850a5b0249f7778a66ff8abce9d103860b
Kelly Sovacool <kellysovacool@gmail.com>
2020-12-10 14:02:20 -0500

    Use list.seqs to get seqs from list file for fraction_mapped everywhere

--- code/py/fraction_mapped_vsearch.py
+++ code/py/fraction_mapped_vsearch.py
@@ -3,21 +3,43 @@
 calculate the fraction of reads that mapped during closed-reference clustering
 """
 
-
-def main():  # TODO: write to log file
-    with open(snakemake.input.count, "r") as count_file:
-        if not snakemake.input.count.endswith(".accnos"):
+def parse_seqs(infilename):
+    with open(infilename, "r") as count_file:
+        if not infilename.endswith(".accnos"):
             next(count_file)  # drop header line if it's not an accnos file
         # first column of all lines
-        all_reads = {line.split("\t")[0].strip() for line in count_file}
-    with open(snakemake.input.list, "r") as list_file:
-        # third column onward of second line, each seq in each OTU delimited by comma
-        next(list_file)
-        line = next(list_file)
-        mapped_reads = {seq for column in line.split()[2:] for seq in column.split(",")}
+        seqs = {line.split("\t")[0].strip() for line in count_file}
+    return seqs
+
+
+def main():
+    query_seqs = parse_seqs(snakemake.input.query)
+    ref_seqs = parse_seqs(snakemake.input.ref)
+    mapped_seqs = parse_seqs(snakemake.input.mapped)
+
+    print('map==ref',mapped_seqs == ref_seqs)
+    print('map==query',mapped_seqs == query_seqs)
+
+    print('len(ref)', len(ref_seqs))
+    print('len(query)', len(query_seqs))
+    print('len(map)', len(mapped_seqs))
+
+    print('ref int query', len(query_seqs.intersection(ref_seqs)))
+    print('map int query', len(mapped_seqs.intersection(query_seqs)))
+    print('map int ref', len(mapped_seqs.intersection(ref_seqs)))
+
+    # remove reference seqs from mapped seqs
+    mapped_seqs = mapped_seqs - ref_seqs
+    print("len(map) after remove ref seqs", len(mapped_seqs))
+
+    # mapped reads should not contain reads not in query
+    assert not (mapped_seqs - query_seqs)
+
     fraction_mapped = round(
-        len(mapped_reads.intersection(all_reads)) / len(all_reads), 3
+        len(mapped_seqs.intersection(query_seqs)) / len(query_seqs), 3
     )
+    assert fraction_mapped <= 1 and fraction_mapped >= 0
+
     wildcards = snakemake.wildcards
     method = wildcards.method
     ref = "NA" if method == "de_novo" else "gg"
