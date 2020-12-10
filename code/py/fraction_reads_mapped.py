5e3b1ad38c21b7c8c8cfa582c1871eaedb9532c6
Kelly Sovacool <kellysovacool@gmail.com>
2020-12-10 12:45:07 -0500

    Use assertions for sanity check

--- code/py/fraction_reads_mapped.py
+++ code/py/fraction_reads_mapped.py
@@ -3,21 +3,41 @@
 calculate the fraction of reads that mapped during closed-reference clustering
 """
 
-
-def main():
-    with open(snakemake.input.count, "r") as count_file:
-        if not snakemake.input.count.endswith(".accnos"):
+def parse_seqs(infilename):
+    with open(infilename, "r") as count_file:
+        if not infilename.endswith(".accnos"):
             next(count_file)  # drop header line if it's not an accnos file
         # first column of all lines
-        all_reads = {line.split("\t")[0].strip() for line in count_file}
-    with open(snakemake.input.list, "r") as list_file:
-        # third column onward of second line, each seq in each OTU delimited by comma
+        seqs = {line.split("\t")[0].strip() for line in count_file}
+    return seqs
+
+
+def parse_list(infilename):
+    with open(infilename, "r") as list_file:
+        # third column onward of second line
+        # each seq in each OTU delimited by comma
         next(list_file)
         line = next(list_file)
-        mapped_reads = {seq for column in line.split()[2:] for seq in column.split(",")}
+        mapped_seqs = {seq for column in line.split()[2:] for seq in column.split(",")}
+    return mapped_seqs
+
+
+def main():
+    query_seqs = parse_seqs(snakemake.input.count)
+    ref_seqs = parse_seqs(snakemake.input.ref)
+    mapped_seqs = parse_list(snakemake.input.list)
+
+    # mapped reads should not contain reference reads
+    assert not mapped_seqs.intersection(ref_seqs)
+
+    # mapped reads should not contain reads not in query
+    assert not (mapped_seqs - query_seqs)
+
     fraction_mapped = round(
-        len(mapped_reads.intersection(all_reads)) / len(all_reads), 3
+        len(mapped_seqs.intersection(query_seqs)) / len(query_seqs), 3
     )
+    assert fraction_mapped <= 1 and fraction_mapped >= 0
+
     wildcards = snakemake.wildcards
     if "sample_frac" in wildcards.__dict__.keys():
         data_str = f"{wildcards.dataset}\t{wildcards.seed}\t{wildcards.method}\t{wildcards.printref}\t{fraction_mapped}\t{wildcards.sample_frac}\t{wildcards.ref_frac}\t{wildcards.ref_weight}\n"
