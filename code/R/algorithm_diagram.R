9c80cb75eb9886e8fcacedf7fcd05c7590965377
Kelly Sovacool <kellysovacool@gmail.com>
2021-03-08 18:14:43 -0500

    Copy Pat's mcc function from OptiClust repo
    source: https://github.com/SchlossLab/Westcott_OptiClust_mSphere_2017/blob/a8bc26855423bba85acc0b8e7cca075e5c94f533/submission/supplemental_text.Rmd#L26-L28

--- code/R/algorithm_diagram.R
+++ code/R/algorithm_diagram.R
@@ -1,16 +1,21 @@
 library(DiagrammeR)
 
-dat <- data.frame(
-  `Seq. 1` = c("D", "F", "G", "H", "I", "I", "J", "J", "N", "O", "P", "P", "P", "Q", "Q"),
-  `Seq. 2` = c("B", "E", "C", "A", "B", "D", "A", "H", "M", "L", "K", "L", "O", "E", "F"),
-  Distance = c(0.024, 0.028, 0.028, 0.027, 0.016, 0.024, 0.028, 0.024, 0.024, 0.024, 0.016, 0.027, 0.027, 0.024, 0.028)
-)
+# Pat's MCC function 
+# source: https://github.com/SchlossLab/Westcott_OptiClust_mSphere_2017/blob/a8bc26855423bba85acc0b8e7cca075e5c94f533/submission/supplemental_text.Rmd#L26-L28
+mcc <- function(tp, tn, fp, fn) {
+   format(round((tp * tn - fp * fn) /	sqrt((tp + fp) * (tp + fn) * (tn + fp) *
+                                              (tn + fn)), digits = 2),
+          digits = 2,
+          nsmall = 2L)
+}
 
-otus <- list(
-  c("A", "B"),
-  c("C", "D", "E"),
-  c("F")
-)
+dat <- data.frame(`Seq. 1` = c("D", "F", "G", "H", "I", "I", "J", "J", "N", "O", "P", "P", "P", "Q", "Q"), 
+                  `Seq. 2` = c("B", "E", "C", "A", "B", "D", "A", "H", "M", "L", "K", "L", "O", "E", "F"), 
+                  Distance = c(0.024, 0.028, 0.028, 0.027, 0.016, 0.024, 0.028, 0.024, 0.024, 0.024, 0.016, 0.027, 0.027, 0.024, 0.028))
+
+otus <- list(c('A', 'B'),
+             c('C', 'D', 'E'),
+             c('F'))
 
 get_otus <- function(idx) {
   x <- otus[[idx]]
@@ -31,8 +36,9 @@ graph [layout = dot,
    3 [label = '@@3'] 
 }
 
-1 -> 3 [dir = back]
+1 -> 3 [dir = back; label = 0.7]
 2 -> 3 [dir = back]
+3 -> 3
 }
 
 [1]: get_otus(1)
