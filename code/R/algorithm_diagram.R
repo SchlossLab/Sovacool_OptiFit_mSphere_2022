50cbb31f6de8ad7e3cc9de41210371f44ff3783a
Kelly Sovacool <kellysovacool@gmail.com>
2021-03-08 18:03:59 -0500

    Add example seq ids to nodes

--- code/R/algorithm_diagram.R
+++ code/R/algorithm_diagram.R
@@ -4,9 +4,14 @@ dat <- data.frame(`Seq. 1` = c("D", "F", "G", "H", "I", "I", "J", "J", "N", "O",
                   `Seq. 2` = c("B", "E", "C", "A", "B", "D", "A", "H", "M", "L", "K", "L", "O", "E", "F"), 
                   Distance = c(0.024, 0.028, 0.028, 0.027, 0.016, 0.024, 0.028, 0.024, 0.024, 0.024, 0.016, 0.027, 0.027, 0.024, 0.028))
 
-otus <- list(1 = c('A','B'),
-             2 = c('C', 'D', 'E'),
-             3 = c('F'))
+otus <- list(c('A', 'B'),
+             c('C', 'D', 'E'),
+             c('F'))
+
+get_otus <- function(idx) {
+   x <- otus[[idx]]
+   paste0(x, collapse = ",")
+}
 
 grViz("
 digraph algorithm {
@@ -18,13 +23,15 @@ graph [layout = dot,
    rank = same
    node [shape = circle]
    1 [label = '@@1'] 
-   2 
-   3
+   2 [label = '@@2'] 
+   3 [label = '@@3'] 
 }
 
 2 -> 3
 1 -> 3 [dir = back]
-
-[1]: otus[[1]]
 }
+
+[1]: get_otus(1)
+[2]: get_otus(2)
+[3]: get_otus(3)
 ", height = 100)
