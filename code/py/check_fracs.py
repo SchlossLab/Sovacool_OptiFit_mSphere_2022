b37708f392967e31be48cc61f4bb3e53e409052d
Kelly Sovacool <kellysovacool@gmail.com>
2021-03-25 10:40:13 -0400

    Fix variable name

--- code/py/check_fracs.py
+++ code/py/check_fracs.py
@@ -5,8 +5,8 @@ import sys
 def main():
     ref_frac = float(sys.argv[1])
     query_frac = float(sys.argv[2])
-    total = ref_frac + sample_frac
-    if ref_frac + sample_frac != 1:
+    total = ref_frac + query_frac
+    if ref_frac + query_frac != 1:
         raise ValueError(f"Fractions do not sum to 1. Sum: {total}")
 
 
