e6e76bba092dea71f93e8b2f60abfa60520279c9
Kelly Sovacool <kellysovacool@gmail.com>
2021-06-23 12:14:43 -0400

    Spell out 'snakemake' for Rmd param

--- code/R/render.R
+++ code/R/render.R
@@ -2,7 +2,7 @@ source(snakemake@input[["fcns"]])
 log_smk()
 rmarkdown::render(
   here::here(snakemake@input[["Rmd"]]),
-  params = list(smk = snakemake),
+  params = list(snakemake = snakemake),
   output_format = snakemake@params[["format"]],
   output_file = here::here(snakemake@output[["file"]])
 )
