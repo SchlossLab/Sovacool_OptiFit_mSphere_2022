d503dfb9ccc097a9a8344044204736eb4d7b81f7
Kelly Sovacool <kellysovacool@gmail.com>
2021-06-23 09:36:08 -0400

    Use stat_summary() & cleanup code

--- code/R/plot_results.R
+++ code/R/plot_results.R
@@ -1,5 +1,4 @@
-
-### data prep
+# plot a summary of the results for all datasets & clustering strategies
 
 set.seed(20210509)
 library(cowplot)
@@ -8,15 +7,6 @@ library(glue)
 library(here)
 library(knitr)
 library(tidyverse)
-
-color_palette <- RColorBrewer::brewer.pal(4, "Dark2")
-dataset_colors <- c(
-  human = color_palette[[3]],
-  marine = color_palette[[1]],
-  mouse = color_palette[[4]],
-  soil = color_palette[[2]]
-)
-tri_colors <-  c("darkorange","darkorchid","cyan4") # https://allisonhorst.github.io/palmerpenguins/articles/intro.html
 dual_colors <- RColorBrewer::brewer.pal(3, 'Set1')[1:2]
 mutate_perf <- function(dat) {
   dat %>% 
@@ -24,39 +14,26 @@ mutate_perf <- function(dat) {
            mem_gb = mem_mb / 1024) %>% 
     rename(sec = s)
 }
-plot_denovo_hline <- function(yint, dat = sum_opticlust) {
-  list(geom_hline(data = dat, aes(yintercept = {{yint}})),
-       labs(caption = "Black line: _de novo_ clustering"),
-       theme(plot.caption = element_markdown())
-  )
-}
 select_cols <- function(dat) {
   dat %>% 
     select(dataset, strategy, method, tool, mcc, sec, mem_gb, fraction_mapped)
 }
-group_sum <- function(dat) {
-  dat %>% 
-    group_by(dataset, tool, method, strategy) %>% 
-    summarize(mcc_median = median(mcc),  # TODO: tidy way to avoid this repetitiveness?
-              sec_median = median(sec),
-              mem_gb_median = median(mem_gb),
-              frac_map_median = median(fraction_mapped))
-}
 
 opticlust <- read_tsv(here('subworkflows/1_prep_samples/results/opticlust_results.tsv')) %>% 
   full_join(read_tsv(here('subworkflows/1_prep_samples/results/dataset_sizes.tsv'))) %>% 
-  mutate_perf() %>% mutate(fraction_mapped = 1, strategy = method)
+  mutate_perf() %>% 
+  mutate(strategy = method)
 sum_opticlust <- opticlust %>% group_sum()
 optifit_dbs <- read_tsv(here('subworkflows/2_fit_reference_db/results/optifit_dbs_results.tsv')) %>% 
   mutate_perf()
 optifit_split <- read_tsv(here('subworkflows/3_fit_sample_split/results/optifit_split_results.tsv')) %>% 
+  filter(ref_frac == 0.5, ref_weight == 'simple') %>% 
   mutate_perf()
 optifit_all <- list(optifit_dbs %>% 
                       mutate(strategy = glue('database_{ref}')),
                     optifit_split %>% 
                       mutate(strategy = 'self-split')) %>% 
   reduce(full_join)
-sum_optifit <- optifit_all %>% group_sum()
 vsearch <- read_tsv(here('subworkflows/4_vsearch/results/vsearch_results.tsv')) %>% 
   mutate_perf() %>% 
   mutate(strategy = case_when(
@@ -66,14 +43,23 @@ mothur_vsearch <- list(optifit_all, opticlust, vsearch) %>%
   lapply(select_cols) %>% 
   reduce(bind_rows) %>% 
   mutate(method = as.character(method),
-         strategy = as.character(strategy))
-sum_all <- mothur_vsearch %>% group_sum()
-head(sum_all)
-
-### make the strategy & method labels prettier
-
-sum_all <- sum_all %>%
+         strategy = as.character(strategy)) %>% 
+  mutate(fraction_mapped = case_when(
+    method %>% as.character() != 'closed'  ~ NA_real_,
+    TRUE                                   ~ fraction_mapped
+  )) %>% 
+  pivot_longer(c(mcc, fraction_mapped, sec), 
+               names_to = 'metric') %>% 
   mutate(
+    metric = factor(
+      case_when(
+        metric == "mcc"        ~ "MCC",
+        metric == 'fraction_mapped'   ~ "Fraction Mapped",
+        metric == 'sec'        ~ "Runtime (sec)",
+        TRUE                          ~ metric
+      ),
+      levels = c('MCC', 'Fraction Mapped', 'Runtime (sec)')
+    ),
     strategy = factor(
       case_when(
         strategy == "de_novo"        ~ "_de novo_",
@@ -89,116 +75,37 @@ sum_all <- sum_all %>%
       case_when(method == "de_novo" ~ "_de novo_",
                 TRUE                ~ method),
       levels = c('open', 'closed', '_de novo_')
-    )
-  )
-
-## plot!
-plot_quality <- function(dat, y_val, title = '') {
-  dat %>% 
-    ggplot(aes(strategy, {{ y_val }}, 
-               color = tool, 
-               shape = method)) + 
-    geom_point(size = 3, position = position_dodge(width = 0.4)) +
-    facet_wrap(dataset ~ ., nrow=1) +
-    scale_shape_manual(values = list(open = 1, closed = 19, `_de novo_` = 17)) +
-    scale_color_manual(values = dual_colors) +
-    scale_y_continuous(labels = c('0', '0.5', '1'), 
-                       breaks = c(0, 0.5, 1),
-                       limits = c(0, 1)) +
-    coord_flip() +
-    labs(x = '', y = '', title = title) +
-    theme_bw() +
-    theme(legend.position="none",
-          axis.text.y = element_markdown(),
-          plot.margin = unit(c(1,1,1,1), 
-                             "points"))
-}
-
-mcc_plot <- sum_all %>% 
-  plot_quality(mcc_median, title = "MCC")
-frac_plot <- sum_all %>% filter(method == 'closed') %>% 
-  plot_quality(frac_map_median, title = "Fraction Mapped")
-
-plot_runtime <- function(dat, yval, title = '') {
-  dat %>% 
-    ggplot(aes(strategy, {{ yval }}, 
-               color = tool, 
-               shape = method
-    )) + 
-    geom_point(size = 3, position = position_dodge(width = 0.4)) +
-    facet_wrap(dataset ~ ., nrow = 1, scales = 'free_x') +
-    scale_shape_manual(values = list(open = 1, closed = 19, `_de novo_` = 17)) +
-    scale_color_manual(values = dual_colors) +
-    coord_flip() +
-    labs(x = '', y = '', title = title) +
-    theme_bw() +
-    guides(color = guide_legend(nrow = 1)) +
-    theme(legend.position = "none",
-          axis.text.y = element_markdown(),
-          plot.margin = unit(c(1,1,1,10), 
-                             "points"))
-}
-runtime_plot <- sum_all %>% plot_runtime(sec_median) + 
-  labs(title = 'Runtime (sec)')
-
-shared_legend <- get_legend(mcc_plot +
-                              guides(color = guide_legend(nrow = 1)) +
-                              theme(legend.position = "bottom",
-                                    legend.title = element_blank(),
-                                    legend.text = element_markdown(),
-                                    plot.margin = unit(c(1,1,1,1), 
-                                                       "points"))
-)
-
-main_plot <- plot_grid(mcc_plot , 
-                       frac_plot, 
-                       runtime_plot,
-                       ncol = 1, align = 'v', labels = 'AUTO'
-) 
-
-plot_grid(main_plot, shared_legend, 
-          ncol = 1, rel_heights = c(1, 0.1))
-
-ggsave(here('figures', 'results.tiff'), 
-       device = 'tiff', dpi=300,
-       width=6, height=6, units='in')
-
-# TODO: jitter only needed for some points, e.g. de novo doesn't need jitter. 
-# TODO: reduce panel gaps/margins
-# TODO: try plotting IQR. note in caption when IQR is smaller than the data points.
-
-# does using facet_grid help reduce whitespace & redundancy?
-sum_all_metrics <- sum_all %>% 
-  mutate(frac_map_median = case_when(
-    method %>% as.character() != 'closed'  ~ NA_real_,
-    TRUE                                   ~ frac_map_median
-  )) %>% 
-  pivot_longer(c(mcc_median, frac_map_median, sec_median), 
-               names_to = 'metric') %>% 
-  mutate(
-    metric = factor(
-      case_when(
-        metric == "mcc_median"        ~ "MCC",
-        metric == 'frac_map_median'   ~ "Fraction Mapped",
-        metric == 'sec_median'        ~ "Runtime (sec)",
-        TRUE                          ~ metric
-      ),
-      levels = c('MCC', 'Fraction Mapped', 'Runtime (sec)')
     ))
-sum_all_metrics %>% 
-  ggplot(aes(strategy, value, color = tool, shape = method)) +
-  geom_point(size = 3) +
-  facet_grid(dataset ~ metric, scales = 'free') +
+
+med_iqr <- function(x) {
+  return(data.frame(y = median(x),
+                    ymin = quantile(x)[2],
+                    ymax = quantile(x)[4]))
+}
+
+mothur_vsearch  %>% 
+  ggplot(aes(value, strategy, color = tool, shape = method)) +
+  # stat_summary(geom = "linerange",
+  #              fun.data = med_iqr,
+  #              position = position_dodge(width = 0.4)) +
+  stat_summary(geom = 'point', 
+               fun = median, 
+               size = 3,
+               position = position_dodge(width = 0.4)) +
+  facet_grid(dataset ~ metric, scales = 'free', switch = 'x') +
   scale_shape_manual(values = list(open = 1, closed = 19, `_de novo_` = 17)) +
   scale_color_manual(values = dual_colors) +
-  coord_flip() +
   labs(x = '', y = '') +
   theme_bw() +
-  theme(legend.position="bottom",
+  theme(strip.placement = "outside",
         axis.text.y = element_markdown(),
         legend.title = element_blank(),
-        legend.text = element_markdown())
+        legend.text = element_markdown(),
+        legend.position="top",
+        legend.margin=margin(t=0, r=0, b=0, l=0, unit='pt'),
+        plot.margin=unit(x=c(3,3,3,0),units="pt")
+  )
 
-ggsave(here('figures', 'results-grid.tiff'), 
+ggsave(here('figures', 'results.tiff'), 
        device = 'tiff', dpi=300,
        width=6, height=6, units='in')
