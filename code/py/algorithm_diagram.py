50119862f91bd62af4e10f750bda2ebfbc753d4c
Kelly Sovacool <kellysovacool@gmail.com>
2021-07-16 10:43:11 -0400

    Reformat with black

--- code/py/algorithm_diagram.py
+++ code/py/algorithm_diagram.py
@@ -8,13 +8,13 @@ def mcc(conf_mat):
     """
     Calculate the Matthews Correlation coefficient given a confusion matrix.
     """
-    tp = conf_mat['tp']
-    tn = conf_mat['tn']
-    fp = conf_mat['fp']
-    fn = conf_mat['fn']
-    return round((tp * tn - fp * fn) /
-                 sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)),
-                 2)
+    tp = conf_mat["tp"]
+    tn = conf_mat["tn"]
+    fp = conf_mat["fp"]
+    fn = conf_mat["fn"]
+    return round(
+        (tp * tn - fp * fn) / sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)), 2
+    )
 
 
 def classify(true_otu, pred_otu):
@@ -23,13 +23,13 @@ def classify(true_otu, pred_otu):
     false positive (fp), or false negataive (fn).
     """
     if true_otu and pred_otu:
-        result = 'tp'
+        result = "tp"
     elif true_otu and not pred_otu:
-        result = 'fn'
+        result = "fn"
     elif not true_otu and pred_otu:
-        result = 'fp'
+        result = "fp"
     elif not true_otu and not pred_otu:
-        result = 'tn'
+        result = "tn"
     else:
         raise ValueError("this should never ever happen")
     return result
@@ -41,10 +41,11 @@ def dist_pairs_to_sets(dframe):
     the distance threshold, to a dict of sets.
     """
     dist_set = defaultdict(set)
-    for seq1, seq2 in zip(dframe['seq1'], dframe['seq2']):
+    for seq1, seq2 in zip(dframe["seq1"], dframe["seq2"]):
         dist_set[seq1].add(seq2)
         dist_set[seq2].add(seq1)
     return dist_set
+
 
 class otuMap(dict):
     def __init__(self, *args, **kwargs):
@@ -64,8 +65,7 @@ class otuMap(dict):
           [{seqA, seqB}, {seqC, secD}, {secE}]
           where the index in the list is the OTU ID.
         """
-        return cls({seq: idx for idx, otu in enumerate(otu_list)
-                             for seq in otu})
+        return cls({seq: idx for idx, otu in enumerate(otu_list) for seq in otu})
 
     @property
     def otu_to_seq(self):
@@ -78,14 +78,20 @@ class otuMap(dict):
             otu_dict_set[otu_id].add(seq)
         return otu_dict_set
 
-    def conf_mat(self, dist_mat, baseline_conf_mat = False):
+    def conf_mat(self, dist_mat, baseline_conf_mat=False):
         """
         :param dist_mat: dict of sets, e.g. {seqA: {seqB, seqC}, seqB: {seqA}}
         :param baseline_conf_mat: provide a confusion matrix (as a dictionary)
           with starting values other than zero.
         """
         # build list of tp, tn, fp, & fn
-        classes = [classify((seq2 in dist_mat[seq1]) or (seq1 in dist_mat[seq2]), self[seq1] == self[seq2]) for seq1, seq2 in combinations(self.keys(), 2)
+        classes = [
+            classify(
+                (seq2 in dist_mat[seq1]) or (seq1 in dist_mat[seq2]),
+                self[seq1] == self[seq2],
+            )
+            for seq1, seq2 in combinations(self.keys(), 2)
+        ]
         # convert to a dictionary of counts
         conf_mat = Counter(classes)
         if baseline_conf_mat:
@@ -93,7 +99,7 @@ class otuMap(dict):
         return conf_mat
 
 
-class OptiFit: # TODO
+class OptiFit:  # TODO
     def __init__(self, ref_otus, query_seqs, dist_mat):
         self.ref_otus = ref_otus
         self.query_seqs = query_seqs
@@ -104,11 +110,43 @@ class OptiFit: # TODO
 
 
 def main():
-    conf_mat = {'tp': 12, 'tn': 1210, 'fp': 0, 'fn': 3}
-    dist_frame = {'seq1': ["D", "F", "G", "H", "I", "I", "J", "J", "N", "O", "P",
-                         "P", "P", "Q", "Q"],
-                  'seq2': ["B", "E", "C", "A", "B", "D", "A", "H", "M", "L", "K",
-                         "L", "O", "E", "F"]}
+    conf_mat = {"tp": 12, "tn": 1210, "fp": 0, "fn": 3}
+    dist_frame = {
+        "seq1": [
+            "D",
+            "F",
+            "G",
+            "H",
+            "I",
+            "I",
+            "J",
+            "J",
+            "N",
+            "O",
+            "P",
+            "P",
+            "P",
+            "Q",
+            "Q",
+        ],
+        "seq2": [
+            "B",
+            "E",
+            "C",
+            "A",
+            "B",
+            "D",
+            "A",
+            "H",
+            "M",
+            "L",
+            "K",
+            "L",
+            "O",
+            "E",
+            "F",
+        ],
+    }
     dist_mat = dist_pairs_to_sets(dist_frame)
 
 
