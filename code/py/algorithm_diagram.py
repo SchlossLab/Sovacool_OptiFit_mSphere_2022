ed6d1f635968616827176502b6dd2c63bb5dcd25
Kelly Sovacool <kellysovacool@gmail.com>
2021-07-16 14:23:17 -0400

    Attempt to represent optifit iterations as dicts of nodes & edges

--- code/py/algorithm_diagram.py
+++ code/py/algorithm_diagram.py
@@ -1,8 +1,10 @@
 """ Prep a series of graphs for the algorithm diagram figure.
 """
 from collections import defaultdict, Counter
+from copy import deepcopy
 from itertools import combinations
 from math import comb, sqrt
+import pandas as pd
 
 
 def opticlust_example():
@@ -86,7 +88,6 @@ class otuMap:
         self.n_seqs = n_seqs
         assert n_seqs >= len(self.seqs_to_otus)
 
-
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.seqs_to_otus)
 
@@ -151,6 +152,10 @@ class otuMap:
         # convert to a dictionary of counts
         return Counter(classes)
 
+    @property
+    def mcc(self):
+        return mcc(self.conf_mat)
+
 
 class OptiFit:
     def __init__(self, ref_otus, query_seqs, query_dist_mat, n_seqs = 0):
@@ -174,12 +179,62 @@ class OptiFit:
         self.fitmap = otuMap(seqs_to_otus = seqs_to_otus,
                              dist_mat = dist_mat,
                              n_seqs = n_seqs)
+        self.iterations = list()  # list of dataframes for ggraph
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__, self.fitmap)
 
-    def run_iter(self): # TODO
-        pass
+    @property
+    def mcc(self):
+        return self.fitmap.mcc
+
+    def iterate(self):
+        return [OptiIter(self.fitmap, seq) for seq in self.query_seqs]
+
+"""
+g1 <-
+  tbl_graph(nodes = data.frame(name = c("A B C", "D E F", "G **H** I", "J K L"),
+                               i = 1:4),
+            edges = data.frame(from = c(2, 2, 2),
+                               to = c(3, 4, 2),
+                               mcc = c(0.97, 0.84, 0.80)) %>%
+              mutate(is_loop = from == to))
+"""
+
+
+class OptiIter:
+    def __init__(self, fitmap, curr_seq):
+        """
+        Calculate possible MCCs if the current seq is moved to different OTUs.
+        Store the nodes and edges as dictionaries for tidygraph.
+        """
+        sim_seqs = fitmap.dist_mat[curr_seq]
+        options = list()
+        self.edges = {'from': [], 'to': [], 'mcc': []}
+        for sim_seq in sim_seqs:
+                option = OptiOption(fitmap, curr_seq, sim_seq)
+                edges['from'].append(option.from)
+                edges['to'].append(option.to)
+                edges['mcc'].append(potential_map.mcc)
+        self.nodes = {'name': [' '.join([s for s in otu
+                                         if s != curr_seq else f"**{s}**"]
+                                         )
+                               for otu in optifit.fitmap.otus_to_seqs.values()],
+                      'id': list(fitmap.otus_to_seqs.keys())}
+
+
+class OptiOption:
+    def __init__(self, curr_otu_map, curr_seq, sim_seq):
+        """
+        Calculate the hypothetical MCC if curr_seq were moved to the OTU containing sim_seq
+        """
+        self.curr_seq = curr_seq
+        self.sim_seq = sim_seq
+        self.from = curr_otu_map.seqs_to_otus[curr_seq]
+        self.to = curr_otu_map.seqs_to_otus[sim_seq]
+        otu_map = deepcopy(curr_otu_map)
+        otu_map.seqs_to_otus[curr_seq] = to
+        self.mcc = otu_map.mcc
 
 
 def main():
@@ -188,6 +243,7 @@ def main():
     query_dist_mat = dist_pairs_to_sets({'seq1': ['X', 'X', 'X', 'X', 'Y'],
                                          'seq2': ['Y', 'C', 'G', 'K', 'C']})
     optifit = OptiFit(ref_otus, query_seqs, query_dist_mat, n_seqs = 53)
+    print(optifit.mcc)
 
 
 if __name__ == "__main__":
