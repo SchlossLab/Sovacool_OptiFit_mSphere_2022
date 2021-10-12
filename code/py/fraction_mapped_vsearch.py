fa5962172f7b933a24501781e2a6acb3eb014031
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-12 09:33:13 -0400

    fraction map sanity check

--- code/py/fraction_mapped_vsearch.py
+++ code/py/fraction_mapped_vsearch.py
@@ -5,16 +5,18 @@ calculate the fraction of reads that mapped during closed-reference clustering
 
 def count_seqs(infilename):
     with open(infilename, "r") as seq_file:
-        num_seqs = sum({1 for line in seq_file if line.startswith('>')})
+        num_seqs = sum(1 for line in seq_file if line.startswith('>'))
     return num_seqs
 
 def get_fraction_mapped(num_query_seqs, num_unmapped_seqs):
-    fraction_mapped = round(num_unmapped_seqs / num_query_seqs, 3)
+    print(num_unmapped_seqs, num_query_seqs)
+    fraction_mapped = 1 - round(num_unmapped_seqs / num_query_seqs, 3)
+    print('frac map:', fraction_mapped)
     assert fraction_mapped <= 1 and fraction_mapped >= 0
     return fraction_mapped
 
 def write_fraction_mapped(wildcards, fraction_mapped):
-    method = wildcards.method
+    method = "closed"#wildcards.method
     ref = "NA" if method == "de_novo" else "gg"
     region = "NA"
     header_line = "dataset\tref\tregion\tmethod\tfraction_mapped\n"
@@ -27,7 +29,6 @@ def main():
     num_query = count_seqs(snakemake.input.query)
     num_unmapped = count_seqs(snakemake.input.unmapped)
     frac_mapped = get_fraction_mapped(num_query, num_unmapped)
-    frac_mapped = 1
     write_fraction_mapped(snakemake.wildcards, frac_mapped)
 
 main()
