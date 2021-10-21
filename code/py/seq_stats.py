34f8dead40e0a8553e0617eec0e2bb0fc9bc8c68
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-21 15:24:19 -0400

    Actually let's get absolute abundance & similarity values

--- code/py/seq_stats.py
+++ code/py/seq_stats.py
@@ -10,11 +10,9 @@ def main(fasta_file, count_file, dist_file, output_file, dissim_thresh=0.03):
             threshold=dissim_thresh,
         )
         with open(output_file, 'w') as file:
-            file.write('seq\trel_abundance\trel_similarity\n')
-            for seq_id, rel_abun, rel_sim in zip(all_seqs.ids,
-                                                 all_seqs.rel_abuns,
-                                                 all_seqs.rel_sims):
-                file.write(f"{seq_id}\t{rel_abun}\t{rel_sim}\n")
+            file.write('seq\tabundance\tsimilarity\n')
+            for seq in all_seqs.seqs:
+                file.write(f"{seq.seq_id}\t{seq.abs_abun}\t{seq.sum_sim}\n")
 
 if __name__ == "__main__":
     main(
