004a82ccb16a5d90b06246e45f182b34b60e71e9
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-24 10:06:40 -0400

    Fix count_table filename instances

--- code/R/mutate_ref_count.R
+++ code/R/mutate_ref_count.R
@@ -1,5 +1,16 @@
 library(dplyr)
-readr::read_tsv(snakemake@input[["count_table"]]) %>%
+
+if (exists('snakemake')) {
+  input_filename <- snakemake@input[["count_table"]]
+  output_filename <- snakemake@output[["count_table"]]
+} else {
+  stop("This script is only intended to be run from a Snakemake workflow")
+  #input_filename <- "data/soil_silva_bact_v4/preclust_db/silva.bact_v4.filter.unique.precluster.pick.count_table"
+  #output_filename <- "data/soil_silva_bact_v4/preclust_db/silva.bact_v4.filter.unique.precluster.pick.mod.count_table"
+}
+
+
+readr::read_tsv(input_filename) %>%
   mutate(dummyRefGroup = total) %>%
   select(Representative_Sequence, total, dummyRefGroup) %>%
-  readr::write_tsv(snakemake@output[["count_table"]])
+  readr::write_tsv(output_filename)
