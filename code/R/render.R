3825142a1f0073fd49e351c24a98d4456fe0cc6a
Kelly Sovacool <kellysovacool@gmail.com>
2021-12-15 10:46:39 -0500

    Allow paper to be knitted outside snakemake

--- code/R/render.R
+++ code/R/render.R
@@ -1,6 +1,6 @@
 rmarkdown::render(
   here::here(snakemake@input[["Rmd"]]),
-  params = list(snakemake = snakemake),
+  params = list(include_figures = TRUE),
   output_format = snakemake@params[["format"]],
   output_file = here::here(snakemake@output[1])
 )
